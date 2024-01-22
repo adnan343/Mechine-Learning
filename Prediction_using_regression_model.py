97f0783f435da9f54f47ef83eca06284b567d2b2
MD. Adnan Hossain Sadi <137903645+adnan343@users.noreply.github.com>
2024-01-22 17:11:17 +0600

    Without extra outputs

--- Prediction_using_regression_model.py
+++ Prediction_using_regression_model.py
@@ -2,24 +2,24 @@
 
 import pandas as pd
 df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/california_housing_train.csv")
-print(df)
+#print(df)
 
 
 #Data Seperation as x and y:
 
 y = df['median_house_value']
-print(y)
+#print(y)
 
 x = df.drop('median_house_value', axis=1)
-print(x)
+#print(x)
 
 
 #Data Splitting:
 
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2, random_state=100)
-print(x_train)
-print(x_test)
+#print(x_train)
+#print(x_test)
 
 
 #Model Building:
@@ -34,8 +34,8 @@ lr.fit(x_train, y_train)
 #Applying the model to make a prediction:
 y_lr_train_pred = lr.predict(x_train)
 y_lr_test_pred = lr.predict(x_test)
-print(y_lr_train_pred)
-print(y_lr_test_pred)
+#print(y_lr_train_pred)
+#print(y_lr_test_pred)
 
 
 #Evaluate model performance:
@@ -50,14 +50,14 @@ lr_train_r2 = r2_score(y_train, y_lr_train_pred)
 lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
 lr_test_r2 = r2_score(y_test, y_lr_test_pred)
 
-print('LR MSE (train): ', lr_train_mse)
-print('LR R2 (train): ', lr_train_r2)
-print('LR MSE (test): ', lr_test_mse)
-print('LR R2 (test): ', lr_test_r2)
+# print('LR MSE (train): ', lr_train_mse)
+# print('LR R2 (train): ', lr_train_r2)
+# print('LR MSE (test): ', lr_test_mse)
+# print('LR R2 (test): ', lr_test_r2)
 
 lr_results = pd.DataFrame(['Linear Regression', lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
 lr_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
-print(lr_results)
+#print(lr_results)
 
 
 #Random Forest:
@@ -71,8 +71,8 @@ rf.fit(x_train, y_train)
 #Applying the model to make a prediction:
 y_rf_train_pred = rf.predict(x_train)
 y_rf_test_pred = rf.predict(x_test)
-print(y_rf_train_pred)
-print(y_rf_test_pred)
+#print(y_rf_train_pred)
+#print(y_rf_test_pred)
 
 
 #Evaluate model performance:
@@ -87,23 +87,26 @@ rf_train_r2 = r2_score(y_train, y_rf_train_pred)
 rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
 rf_test_r2 = r2_score(y_test, y_rf_test_pred)
 
-print('RF MSE (train): ', rf_train_mse)
-print('RF R2 (train): ', rf_train_r2)
-print('RF MSE (test): ', rf_test_mse)
-print('RF R2 (test): ', rf_test_r2)
+# print('RF MSE (train): ', rf_train_mse)
+# print('RF R2 (train): ', rf_train_r2)
+# print('RF MSE (test): ', rf_test_mse)
+# print('RF R2 (test): ', rf_test_r2)
 
 rf_results = pd.DataFrame(['Random Forest', rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
 rf_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
-print(rf_results)
+#print(rf_results)
 
 
 # Model comparison:
 
 df_models = pd.concat([lr_results, rf_results], axis=0)
-print()
+#print()
 
 df_models.reset_index(drop=True)
-print(df_models)
+#pd.set_option('display.max_columns', None)
+from tabulate import tabulate
+print(tabulate(df_models, headers='keys'))
+#print(df_models)
 
 
 #Data Visualization of prediction results:
